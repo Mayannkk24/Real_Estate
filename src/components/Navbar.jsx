7b4ae636a6c61b664641db13447a0757704aaa51
Mayank Goley <Mayankgoley2408@gmail.com>
2026-01-06 20:14:30 +0530

    Add About section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {assets} from '../assets/assets'
 import { useState } from 'react'
 
 const Navbar = () => {
   const [ShowMobileMenu, setShowMobileMenu] = useState(false)
+
+   useEffect(()=>{
+    if(ShowMobileMenu){
+      document.body.style.overflow ='hidden'
+    }else{
+      document.body.style.overflow ='auto'
+    } return ()=>{
+      document.body.style.overflow ='auto'
+    }
+
+   },[ShowMobileMenu])
+
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
        <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
@@ -23,10 +35,10 @@ const Navbar = () => {
           <img onClick={() => setShowMobileMenu(false) } src={assets.cross_icon} className='w-6' alt=''/>
         </div>
         <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
-          <a href='#Header' className='px-4 py-2 rounded-full inline-block'>Home</a>
-           <a href='#About' className='px-4 py-2 rounded-full inline-block'>About</a>
-            <a href='#Projects' className='px-4 py-2 rounded-full inline-block'>Projects</a>
-             <a href='#Testimonials' className='px-4 py-2 rounded-full inline-block'>Testimonials</a>
+          <a onClick={() => setShowMobileMenu(false)} href='#Header' className='px-4 py-2 rounded-full inline-block'>Home</a>
+           <a onClick={() => setShowMobileMenu(false)} href='#About' className='px-4 py-2 rounded-full inline-block'>About</a>
+            <a onClick={() => setShowMobileMenu(false)} href='#Projects' className='px-4 py-2 rounded-full inline-block'>Projects</a>
+             <a onClick={() => setShowMobileMenu(false)} href='#Testimonials' className='px-4 py-2 rounded-full inline-block'>Testimonials</a>
         </ul>
       </div>
     </div>
